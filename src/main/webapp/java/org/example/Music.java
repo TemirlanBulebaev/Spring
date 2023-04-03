f8b161bc2fcecacae42aabfbd73e29410f8a3b8c
temirlan_bulebaev <tbulebaev@bk.ru>
2023-04-03 23:24:16 +0500

    Dependency Injection

--- src/main/webapp/java/org/example/Music.java
+++ src/main/webapp/java/org/example/Music.java
@@ -1,4 +1,4 @@
-package org.example;
+ package org.example;
 
 public interface Music {
     String getSong();
--- src/main/webapp/java/org/example/MusicPlayer.java
+++ src/main/webapp/java/org/example/MusicPlayer.java
@@ -2,6 +2,14 @@ package org.example;
 
 public class MusicPlayer {
     private Music music;
-    
-    public MusicPlayer(Music music);
+
+    //Ioc
+    public MusicPlayer(Music music){
+        this.music = music;
+    }
+
+    public void playMusic(){
+        System.out.println("Playing: " + music.getSong());
+    }
+
 }
